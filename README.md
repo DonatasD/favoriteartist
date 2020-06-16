3d29a20f137c7e932729143a38ea12b9ca17c948
DonatasD <donatasdaubar@gmail.com>
2020-06-17 01:13:02 +0800

    Init with spring initializer

---
+++ README.md
@@ -0,0 +1,2 @@
+# Favorite Artist
+Interview assignment favorite artist
---
+++ src/test/java/com/donatasd/favoriteartist/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.donatasd.favoriteartist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
